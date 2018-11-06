69eb422d8a5b4d69d9fc81f4a2f0fdea614f844e
Catherine <Catherine25@users.noreply.github.com>
2018-11-06 19:16:35 +0200

    Data's access modifiers changed; Added comments;

--- WindowsFormsApp1/Controller.cs
+++ WindowsFormsApp1/Controller.cs
@@ -1,32 +1,30 @@
 ﻿using System;
 using System.IO;
 
-namespace WindowsFormsApp1 {
-    class Controller {
+namespace WindowsFormsApp1
+{
+    class Controller
+    {
         //constructor
-        public Controller() {
+        public Controller()
+        {
             FilesPath = @"c:\New Folder\";
             Random = new Random();
         }
+
         //checking if file exists
-        public void FileExists(string name) {
+        public void FileExists(string name)
+        {
             FileInfo fileInf = new FileInfo(FilesPath + name);
             if (!fileInf.Exists)
                 FillFile(name);
         }
-        //fill SA SB SC
-        void FillFile(string name) {
-            using (StreamWriter sw = new StreamWriter(FilesPath + name, false, System.Text.Encoding.Default)) {
-                string s = "";
-                // filling string
-                for (int i = 0; i < 10; i++)
-                    s += Random.Next(0, 100).ToString() + " ";
-                sw.Write(s);
-            } //dispose file   
-        }
-        // getting from file by adding name
-        public void GetFromFile(string name) {
-            using (StreamReader sr = new StreamReader(FilesPath + name, System.Text.Encoding.Default)) {
+
+        //getting from file by adding name
+        public void GetFromFile(string name)
+        {
+            using (StreamReader sr = new StreamReader(FilesPath + name, System.Text.Encoding.Default))
+            {
                 string file = sr.ReadToEnd();
                 if (name == "SA.txt")
                     SA = File.ReadAllText(FilesPath + name).Split(' ');
@@ -34,14 +32,17 @@ namespace WindowsFormsApp1 {
                     SB = File.ReadAllText(FilesPath + name).Split(' ');
                 if (name == "SC.txt")
                     SC = File.ReadAllText(FilesPath + name).Split(' ');
-            } //Dispose file 
+            }
         }
-        //Moves all elements to SD
-        public void ToSD() {
+
+        //moves all elements to SD
+        public void ToSD()
+        {
             //finding min size of three files
             SortedIndex = Math.Min(Math.Min(SA.Length, SB.Length), SC.Length);
             //writing all three to SD 
-            for (int i = 0; i < SortedIndex; i++) {
+            for (int i = 0; i < SortedIndex; i++)
+            {
                 SD += SA[i];
                 SD += " ";
                 SD += SB[i];
@@ -50,15 +51,31 @@ namespace WindowsFormsApp1 {
                 SD += " ";
             }
             //if files have not same size
-            if (SA.Length != SB.Length || SA.Length != SC.Length) {
+            if (SA.Length != SB.Length || SA.Length != SC.Length)
+            {
                 if (DifferentSize() == 2) //one are passed
                     TwoInOne();
                 if (DifferentSize() == 1) //two are passed
                     LastInOne();
             }
         }
+
+        //fill SA SB SC
+        void FillFile(string name)
+        {
+            using (StreamWriter sw = new StreamWriter(FilesPath + name, false, System.Text.Encoding.Default))
+            {
+                string s = "";
+                // filling string
+                for (int i = 0; i < 10; i++)
+                    s += Random.Next(0, 100).ToString() + " ";
+                sw.Write(s);
+            }  
+        }
+
         //Counts how many arrays need to be passed
-        int DifferentSize() { 
+        int DifferentSize()
+        {
             int[] IsPassed = new int[3] { 0, 0, 0 };
             if (SA.Length == SortedIndex)
                 IsPassed[0]++;
@@ -69,29 +86,37 @@ namespace WindowsFormsApp1 {
             return IsPassed[0] + IsPassed[1] + IsPassed[2];
         }
 
-        void TwoInOne() {
+        //moves 2 arrays' data in SD
+        void TwoInOne()
+        {
             int NextStep = 0;
-            if (SortedIndex == SA.Length) {
+            if (SortedIndex == SA.Length)
+            {
                 NextStep = Math.Min(SB.Length, SC.Length);
-                for (int i = SortedIndex; i < NextStep; i++) {
+                for (int i = SortedIndex; i < NextStep; i++)
+                {
                     SD += SB[i];
                     SD += " ";
                     SD += SC[i];
                     SD += " ";
                 }
             }
-            if (SortedIndex == SB.Length) {
+            if (SortedIndex == SB.Length)
+            {
                 NextStep = Math.Min(SA.Length, SC.Length);
-                for (int i = SortedIndex; i < NextStep; i++) {
+                for (int i = SortedIndex; i < NextStep; i++)
+                {
                     SD += SA[i];
                     SD += " ";
                     SD += SC[i];
                     SD += " ";
                 }
-            }  
-            if (SortedIndex == SC.Length) {
+            }
+            if (SortedIndex == SC.Length)
+            {
                 NextStep = Math.Min(SA.Length, SB.Length);
-                for (int i = SortedIndex; i < NextStep; i++) {
+                for (int i = SortedIndex; i < NextStep; i++)
+                {
                     SD += SA[i];
                     SD += " ";
                     SD += SB[i];
@@ -101,36 +126,42 @@ namespace WindowsFormsApp1 {
             SortedIndex = NextStep;
         }
 
-        void LastInOne() {
+        //moves last array's data to SD
+        void LastInOne()
+        {
             int Last = Math.Max(Math.Max(SA.Length, SB.Length), SC.Length);
             if (Last == SA.Length)
-                for (int i = SortedIndex; i < Last; i++) {
+                for (int i = SortedIndex; i < Last; i++)
+                {
                     SD += SA[i];
                     SD += " ";
                 }
             if (Last == SB.Length)
-                for (int i = SortedIndex; i < Last; i++) {
+                for (int i = SortedIndex; i < Last; i++)
+                {
                     SD += SB[i];
                     SD += " ";
                 }
             if (Last == SC.Length)
-                for (int i = SortedIndex; i < Last; i++) {
+                for (int i = SortedIndex; i < Last; i++)
+                {
                     SD += SC[i];
                     SD += " ";
                 }
             SortedIndex = Last;
         }
 
-        public void SaveResult() {
+        //saves result to SD
+        public void SaveResult()
+        {
             using (StreamWriter sw = new StreamWriter(FilesPath + "SD.txt", false, System.Text.Encoding.Default))
                 sw.Write(SD); //dispose file
         }
 
         //data
         string[] SA, SB, SC;
-        int SortedIndex { get; set; }
-        string SD;
-        string FilesPath { get; set; }
-        Random Random { get; }
+        int SortedIndex;
+        string SD, FilesPath;
+        Random Random;
     }
 }
